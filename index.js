421fc116070bd0b5e35f11ec2d57eea851ab1d81
Marcos de Barros Azevedo <mbarrosazevedo@gmail.com>
2024-07-05 10:17:13 -0300

    First commit

---
+++ index.js
@@ -0,0 +1,39 @@
+const fs = require('fs');
+const sharp = require('sharp');
+
+const inputFolder = './input';
+const outputFolder = './output';
+const prefixoPersonalizado = '';
+
+// Ensure the output folder exists
+if (!fs.existsSync(outputFolder)) {
+    fs.mkdirSync(outputFolder);
+  }
+  
+  // List all images in the input folder
+  fs.readdir(inputFolder, (err, files) => {
+    if (err) {
+      console.error('Error reading input folder:', err);
+      return;
+    }
+  
+    // Iterate over each file in the input folder
+    files.forEach((file) => {
+      const inputPath = `${inputFolder}/${file}`;
+      const outputFileName = `${prefixoPersonalizado}${file.replace(/\..+$/, '.webp')}`; // Nome do arquivo de saída com prefixo personalizado
+        const outputPath = `${outputFolder}/${outputFileName}`;
+
+  
+      // Convert and compress the image to WebP with 80% quality (adjust as needed)
+      sharp(inputPath)
+        .webp({ quality: 100 })
+        .toFile(outputPath, (err) => {
+          if (err) {
+            console.error(`Error converting ${file}:`, err);
+          } else {
+            console.log(`${file} successfully converted and compressed to WebP.`);
+          }
+        });
+    });
+  });
+
